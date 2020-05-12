a74cc219195c6900bb922962b6c864b81f7777ab
Matthew Epland <matthewepland@gmail.com>
2020-05-11 22:52:53 -0400

    reworked plotting code to make output images look more like a standard EKG display, no axis ticks, grid lines with the correct spacing

--- preprocessing/plotting.py
+++ preprocessing/plotting.py
@@ -19,13 +19,13 @@ mpl.rcParams['xtick.labelsize']     = 13
 mpl.rcParams['ytick.labelsize']     = 13
 mpl.rcParams['xtick.minor.visible'] = True
 mpl.rcParams['ytick.minor.visible'] = True
-mpl.rcParams['xtick.major.width']   = 0.8  # major tick width in points
+mpl.rcParams['xtick.major.width']   = 1.  # major tick width in points
 mpl.rcParams['xtick.minor.width']   = 0.8  # minor tick width in points
 mpl.rcParams['xtick.major.size']    = 7.0  # major tick size in points
 mpl.rcParams['xtick.minor.size']    = 4.0  # minor tick size in points
 mpl.rcParams['xtick.major.pad']     = 1.5  # distance to major tick label in points
 mpl.rcParams['xtick.minor.pad']     = 1.4  # distance to the minor tick label in points
-mpl.rcParams['ytick.major.width']   = 0.8  # major tick width in points
+mpl.rcParams['ytick.major.width']   = 1.  # major tick width in points
 mpl.rcParams['ytick.minor.width']   = 0.8  # minor tick width in points
 mpl.rcParams['ytick.major.size']    = 7.0  # major tick size in points
 mpl.rcParams['ytick.minor.size']    = 4.0  # minor tick size in points
@@ -49,20 +49,24 @@ colors = prop_cycle.by_key()['color']
 # aspect_ratio_multi = 1.
 
 plot_png=True
-
 plot_jpg=False
 
 ########################################################
-def plot_waveform(df, channel_names, m_path='output', fname='waveform', tag='', inline=False, target_period=5):
+def plot_waveform(dfp, channel_names, sampling_rate, m_path='output', fname='waveform', tag='', inline=False, target_time_range=5):
 
-	target_res = 800
+	# target_res = 1200 # decent quality
+	target_res = 800 # hackathon setting
 	size_in = 20
 
 	png_dpi = target_res/size_in
 	jpg_dpi = png_dpi
 
+	major_x = 0.2  # seconds
+	minor_x = 0.04 # seconds
+	major_y = 0.5 # mV
+	minor_y = 0.1 # mV
 
-	fig, axs = plt.subplots(len(channel_names), sharex=True, sharey=True)
+	fig, axs = plt.subplots(len(channel_names), sharex=True, sharey=False)
 
 	# this_vsize = 20
 	# this_aspect_ratio = 10. / 12. # width / height
@@ -71,18 +75,75 @@ def plot_waveform(df, channel_names, m_path='output', fname='waveform', tag='', 
 	# fig.set_size_inches(target_res / png_dpi, target_res / png_dpi)
 	fig.set_size_inches(size_in, size_in)
 
-	x = np.linspace(0., float(target_period), len(df.index))
+	n_samples = len(dfp.index)
+
+	data_time_range = n_samples / sampling_rate
+	if target_time_range <= data_time_range:
+		# we have enough samples to fill the requested target_time_range
+		time_range = target_time_range
+	else:
+		# we do NOT have enough samples to fill the requested target_time_range, use all of the data we have
+		time_range = data_time_range
+
+	n_samples_to_use = int(np.floor(sampling_rate*time_range))
+
+	x = np.linspace(0., float(time_range), n_samples_to_use)
 
 	for ichannel,channel_name in enumerate(channel_names):
-		axs[ichannel].plot(x, df[channel_name], c=colors[ ichannel % len(colors) ])
+		axs[ichannel].plot(x, dfp[channel_name].iloc[0:n_samples_to_use], c=colors[ ichannel % len(colors) ])
 
-		axs[ichannel].grid(which='both', axis='both')
-
-		axs[ichannel].set_ylabel(channel_name)
+		axs[ichannel].set_ylabel(f'{channel_name} ', rotation='horizontal', ha='right')
 		if ichannel == len(channel_names) - 1:
 			axs[ichannel].set_xlabel('Time [S]')
 
-		axs[ichannel].set_xlim([0.,float(target_period)])
+		axs[ichannel].grid(which='major', axis='both', color='#CCCCCC', alpha=1., lw=1)
+		axs[ichannel].grid(which='minor', axis='both', color='#CCCCCC', alpha=0.5, lw=0.6)
+
+		axs[ichannel].xaxis.set_ticks_position('none')
+		axs[ichannel].yaxis.set_ticks_position('none')
+
+	axs[0].set_xlim([0.,float(time_range)])
+
+	n_ticks_major_x = int(np.ceil(time_range / major_x))
+	x_major_ticks = np.linspace(0., time_range, n_ticks_major_x+1)
+
+	n_ticks_minor_x = int(np.ceil(time_range / minor_x))
+	x_minor_ticks = np.linspace(0., time_range, n_ticks_minor_x+1)
+
+	axs[0].set_xticks(x_major_ticks)
+	axs[0].set_xticks(x_minor_ticks, minor=True)
+
+	for ichannel in range(len(channel_names)):
+		_y_min_auto, _y_max_auto = axs[ichannel].get_ylim()
+		if ichannel == 0 or _y_min_auto < y_min_auto:
+			y_min_auto = _y_min_auto
+		if ichannel == 0 or y_max_auto < _y_max_auto:
+			y_max_auto = _y_max_auto
+
+	y_min = minor_y * (round(y_min_auto / minor_y)-1)
+	y_max = minor_y * (round(y_max_auto / minor_y)+1)
+
+	if y_min < 0:
+		int_ticks_major_y_min = int(np.ceil( y_min / major_y))
+		int_ticks_minor_y_min = int(np.ceil( y_min / minor_y))
+	else:
+		int_ticks_major_y_min = int(np.floor(y_min / major_y))
+		int_ticks_minor_y_min = int(np.floor(y_min / minor_y))
+
+	if y_max < 0:
+		int_ticks_major_y_max = int(np.ceil(y_max / major_y))
+		int_ticks_minor_y_max = int(np.ceil(y_max / minor_y))
+	else:
+		int_ticks_major_y_max = int(np.floor( y_max / major_y))
+		int_ticks_minor_y_max = int(np.floor( y_max / minor_y))
+
+	y_major_ticks = np.linspace(major_y*int_ticks_major_y_min, major_y*int_ticks_major_y_max, int_ticks_major_y_max-int_ticks_major_y_min+1)
+	y_minor_ticks = np.linspace(minor_y*int_ticks_minor_y_min, minor_y*int_ticks_minor_y_max, int_ticks_minor_y_max-int_ticks_minor_y_min+1)
+
+	for ichannel in range(len(channel_names)):
+		axs[ichannel].set_ylim([y_min,y_max])
+		axs[ichannel].set_yticks(y_major_ticks)
+		axs[ichannel].set_yticks(y_minor_ticks, minor=True)
 
 	plt.tight_layout()
 	if inline:
