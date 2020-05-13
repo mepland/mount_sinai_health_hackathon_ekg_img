e3f12a18c4410ae1ce3a0755ddb2d72c9567bb8c
Matthew Epland <matthewepland@gmail.com>
2020-05-13 01:40:56 -0400

    Fixed bugs and changed naming schema. Now working with multiprocessing!

--- preprocessing/ekg_preprocessing.py
+++ preprocessing/ekg_preprocessing.py
@@ -23,17 +23,20 @@ from plotting import * # load plotting code
 def process_tranche(in_path, out_path, im_res, slice_time_range, n_slices_max, sampling_freq, channel_names, tranche):
 	n_channels = len(channel_names)
 	n_samples_per_slice = int(np.ceil(sampling_freq*slice_time_range))
-	n_wf_counter_dict = {}
+	obs_Dx = []
 
-	tranche_name = tranche['name']
+	tranche_number = tranche['number']
 
 	for fname in tranche['fnames']:
-		fname = f'{in_path}/{fname}'
+		path_fname = f'{in_path}/{fname}'
 		try:
-			x = loadmat(f'{fname}.mat')['val']
+			x = loadmat(f'{path_fname}.mat')['val']
 
-			with open(f'{fname}.hea', 'r') as f:
-				header_data = f.readlines()
+			with open(f'{path_fname}.hea', 'r') as f:
+				header_data_raw = f.readlines()
+
+			# clean any whitespace to single spaces
+			header_data = [' '.join(l.split()) for l in header_data_raw]
 
 			# get gain and offset per channel
 			gain_channel = np.zeros(n_channels)
@@ -59,9 +62,9 @@ def process_tranche(in_path, out_path, im_res, slice_time_range, n_slices_max, s
 			starts = (1. - (1. / float(n_slices)) )*np.random.random(n_slices)
 
 			m_path = f'{out_path}/{Dx}'
-			if Dx not in n_wf_counter_dict.keys():
-				n_wf_counter_dict[Dx] = 0
+			if Dx not in obs_Dx:
 				os.makedirs(m_path, exist_ok=True)
+				obs_Dx.append(Dx)
 
 			# print each slice
 			for islice in range(n_slices):
@@ -71,16 +74,15 @@ def process_tranche(in_path, out_path, im_res, slice_time_range, n_slices_max, s
 				plot_waveform(dfp_channels.iloc[i_slice_start:i_slice_stop],
 					channel_names, sampling_freq,
 					m_path=m_path,
-					fname=f'wf_{tranche_name}-{n_wf_counter_dict[Dx]}-{islice}',
+					# fname=f't{tranche_number:04d}-{fname}-s{islice:02d}',
+					fname=f'{fname}-s{islice:02d}',
 					tag='', inline=False,
 					target_time_range=slice_time_range, target_im_res=im_res,
 					run_parallel=True, # Turn off some error checking to speed things up
 				)
 
-				n_wf_counter_dict[Dx] += 1
-
 		except (Exception, Warning) as err:
-			error_msg = f'Error processing fname = {fname} in tranche_name = {tranche_name}!\n{str(err)}'
+			error_msg = f'Error processing fname = {fname} in tranche_number = {tranche_number}!\n{str(err)}'
 			print(error_msg)
 			with open('./preprocessing.log', 'a') as f:
 				f.write(f'{error_msg}\n')
@@ -96,7 +98,7 @@ if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser(description='Author: Matthew Epland', formatter_class=lambda prog: CustomFormatter(prog, max_help_position=30))
 
-	parser.add_argument('-i', '--input_path', dest='input_path', type=str, default='../data/Training_Set_2', help='Path to top level directory containing the PhysioNet data.')
+	parser.add_argument('-i', '--input_path', dest='input_path', type=str, default='../data/PhysioNetChallenge2020_Training_CPSC/Training_WFDB', help='Path to top level directory containing the PhysioNet data.')
 	parser.add_argument('-o', '--output_path', dest='output_path', type=str, default='./output', help='Path to output directory. Will actually save to a subdirectory named im_res_{im_res}.')
 	parser.add_argument('-n', '--n_ekg_to_process', dest='n_ekg_to_process', type=int, default=-1, help='Number of input EKGs to process, -1 is all.')
 	parser.add_argument('-s', '--size', dest='im_res', type=int, default=800, help='Size of output image (800 produces a 800x800 image).')
@@ -104,7 +106,7 @@ if __name__ == '__main__':
 	parser.add_argument('--n_slices_max', dest='n_slices_max', type=int, default=5, help='Maximum number of slices to take from a single EKG.')
 	parser.add_argument('--sampling_freq', dest='sampling_freq', type=int, default=500, help='EKG ADC sampling frequency (Hz).')
 	parser.add_argument('-j', '--processes', dest='n_processes', type=int, default=1, help='Number of sub-processes run in parallel.')
-	parser.add_argument('--n_tranches', dest='n_tranches', type=int, default=-1, help='Number of tranches to create for parallel processing, -1 creates 5*n_processes.')
+	parser.add_argument('--n_tranches', dest='n_tranches', type=int, default=-1, help='Number of tranches to create for parallel processing, -1 creates 100*n_processes.')
 	parser.add_argument('--seed', dest='rnd_seed', type=int, default=42, help='Random seed for reproducibility.')
 	parser.add_argument('-v','--verbose', dest='verbose', action='count', default=0, help='Enable verbose output.')
 
@@ -133,12 +135,12 @@ if __name__ == '__main__':
 		raise ValueError(f'Trying to use {n_processes} processes but only have {n_cores} cores!')
 
 	if n_tranches <= 0:
-		n_tranches = 5*n_processes
+		n_tranches = 100*n_processes
 
 	# setup variables
 	channel_names = ['I', 'II', 'III', 'aVR', 'aVL', 'aVF', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6'] # TODo hard coded
 
-	output_path = f'{output_path}/im_res_{im_res}' # TODo hard coded
+	output_path = f'{output_path}/im_res_{im_res}'
 
 	np.random.seed(rnd_seed)
 
@@ -161,21 +163,21 @@ if __name__ == '__main__':
 	n_fnames_per_tranche = int(np.ceil(n_ekg_to_process / n_tranches))
 
 	tranches = []
-	for itranches in range(n_tranches):
-		start = itranches*n_fnames_per_tranche
+	for itranche in range(n_tranches):
+		start = itranche*n_fnames_per_tranche
 		stop = start + n_fnames_per_tranche
 
-		if itranches != n_tranches-1:
+		if itranche != n_tranches-1:
 			tranche_fnames = fnames[start:stop]
 		else:
 			tranche_fnames = fnames[start:]
 
-		tranches.append({'name': f't{itranches:02d}', 'fnames': tranche_fnames})
+		tranches.append({'number': itranche, 'fnames': tranche_fnames})
 
 	if verbose:
 		print(f'n_ekg_to_process = {n_ekg_to_process}, n_tranches = {n_tranches}, n_fnames_per_tranche = {n_fnames_per_tranche}')
 		for t in tranches:
-			print(f"Tranche {t['name']} contains {len(t['fnames'])} fnames")
+			print(f"Tranche {t['number']} contains {len(t['fnames'])} fnames")
 
 	########################################################
 	# actually run
@@ -184,7 +186,7 @@ if __name__ == '__main__':
 
 	process_tranche_partial = partial(process_tranche, input_path, output_path, im_res, slice_time_range, n_slices_max, sampling_freq, channel_names)
 
-	# process_tranche_partial({'name': 'DEBUG', 'fnames': [f for i,f in enumerate(tranches[0]['fnames']) if i < 30]}) # single threaded debugging
+	# process_tranche_partial({'number': 0, 'fnames': [f for i,f in enumerate(tranches[0]['fnames']) if i < 30]}) # single threaded debugging
 
 	for _ in tqdm(pool.imap_unordered(process_tranche_partial, tranches), total=len(tranches), desc='Tranches'):
 		pass
